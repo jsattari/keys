aadc9434ba97dd1f0e8fc1a5ae74c29653b3df10
John Sattari <johnsattari@johns-air.lan>
2023-02-04 23:04:23 -0800

    added tests for removed chars and no repeating chars

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,10 +7,37 @@ from click.testing import CliRunner
 runner = CliRunner()
 
 
+# test for lengths
 @pytest.mark.parametrize(
-    "cmd, results", [("-l 12", 12), ("-l 6", 6), ("-l 22", 22), ("-l 31", 31)]
+    "cmd, solution", [("-l 12", 12), ("-l 6", 6), ("-l 22", 22), ("-l 31", 31)]
 )
-def test_main_length(cmd, results):
+def test_main_lengths(cmd, solution) -> None:
     result = runner.invoke(main, cmd)
     assert result.exit_code == 0
-    assert len(result.output[:-1]) == results
+    assert len(result.output[:-1]) == solution
+
+
+# test for removed chars
+@pytest.mark.parametrize(
+    "cmd, solution",
+    [
+        ("-r 'xyz'", True),
+        ("-r '123'", True),
+        ("-r '1A2B3C'", True),
+        ("-r '%@!!'", True),
+    ],
+)
+def test_remove_characters(cmd, solution) -> None:
+    result = runner.invoke(main, cmd)
+    chars = cmd.split(" ")[1]
+    assert result.exit_code == 0
+    assert all([x not in result.output[:-1] for x in chars]) == solution
+
+
+@pytest.mark.parametrize("cmd, solution", [("-n", True)])
+def test_no_repeats(cmd, solution) -> None:
+    result = runner.invoke(main, cmd)
+    assert result.exit_code == 0
+    assert (
+        len(set(result.output[:-1])) == len(result.output[:-1])
+    ) == solution  # noqa: E501
